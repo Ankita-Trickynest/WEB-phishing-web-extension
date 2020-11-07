edce287bf97311a06a76c4b3c76bfb672f8376c3
ANKITA <trickynest1220@gmail.com>
2020-11-07 18:42:36 +0530

    initial commit

---
+++ MAIN/Flask/app.py
@@ -0,0 +1,59 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from PIL import Image
+#importing the inputScript file used to analyze the URL
+import inputScript
+import os
+PEOPLE_FOLDER = os.path.join('static', 'img')
+
+
+
+#load model
+app = Flask(__name__)
+model = pickle.load(open('Phishing_Website.pkl', 'rb'))
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+
+#Redirects to the page to give the user iput URL.
+@app.route('/predict')
+def predict():
+
+    return render_template('final.html')
+
+#Fetches the URL given by the URL and passes to inputScript
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    url = request.form['URL']
+    checkprediction = inputScript.main(url)
+    prediction = model.predict(checkprediction)
+    print(prediction)
+    output=prediction[0]
+
+    if(output==1):
+        pred="Your are safe!!  This is a Legitimate Website."
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'd.gif')
+    else:
+        pred="You are on the wrong site. Be cautious!"
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'c.gif')
+    return render_template('final.html', prediction_text='{}'.format(pred),url=url,user_image = full_filename)
+
+#Takes the input parameters fetched from the URL by inputScript and returns the predictions
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
